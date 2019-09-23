f6d93aaa780dd5b4e19c65a9ec85c02d1a6d8f0d
Isabela Santos <isabelatravaglia@gmail.com>
2019-09-23 16:04:23 +0100

    added seed. Added dependent:destroy to models movie and user.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
-  has_many :watchlists
+  has_many :watchlists, dependent: :destroy
   mount_uploader :photo, PhotoUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one :watchlist
+  has_one :watchlist, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
