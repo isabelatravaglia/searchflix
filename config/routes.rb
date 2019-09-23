f6a7435bbd7438b8f565214292f509db7d7b758a
Isabela Santos <isabelatravaglia@gmail.com>
2019-09-23 16:32:59 +0100

    added movies controller (index). added route for movies index. Added cards (scss) for movies and changed view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'movies/index'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'movies#index'
+
+  resources :movies, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
