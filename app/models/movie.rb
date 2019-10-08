7d9f5a02756f24a49f736174be424c380d43df63
Isabela Santos <isabelatravaglia@gmail.com>
2019-10-08 19:19:20 +0100

    added pgsearch gem for search feature. Added search form on navbar. Changed Movies controller to add query params on index and changed Movie model to add pgsearch. Also made a minor change on seeds to display the year information.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,11 @@ class Movie < ApplicationRecord
   has_many :watchlists, dependent: :destroy
   mount_uploader :photo, PhotoUploader
   default_scope { order('created_at DESC') }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_plot,
+    against: [:title, :plot],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
