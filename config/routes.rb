75a55c078c1bb3b22c8fe28db6776780e4746398
Isabela Santos <isabelatravaglia@gmail.com>
2019-10-12 14:27:48 +0100

    added basic changes for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'movies/index'
+  # get 'movies/index'
   devise_for :users
-  root to: 'movies#index'
+  root to: 'pages#home'
 
   resources :movies, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
